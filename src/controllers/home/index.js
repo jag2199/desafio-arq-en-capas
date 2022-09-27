d90c685386a58c2d19fc712124dec0b8e40803be
Jonathan <aguss997779@gmail.com>
2022-09-27 17:27:38 -0300

    Arquitectura en capas fix

--- src/controllers/home/index.js
+++ src/controllers/home/index.js
@@ -2,12 +2,12 @@ import ProductosContainer from "../daos/productos.js"
 
 let container = new ProductosContainer()
 
-const getHome = (req, res) => {
-    res.render("index", { productos: container.getAll(), nombre: req.user.nombre })
+const getHome = async (req, res) => {
+    res.render("index", { productos: await container.getAll(), nombre: req.user.nombre })
 }
 
-const postHome = (req, res) => {
-    container.save(req.body)
+const postHome = async (req, res) => {
+    await container.save(req.body)
     res.redirect("/")
 }
 
